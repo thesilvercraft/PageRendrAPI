d551aaf2c9e31b686a76ca6ff18d4410a0ef0185
SilverDimond <46320280+Silverdimond@users.noreply.github.com>
2021-09-27 20:17:43 +0200

    So apparently the controler gets created each time the thing is called so yeah

--- PageRendrAPI/Controllers/RenderController.cs
+++ PageRendrAPI/Controllers/RenderController.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using SDBrowser;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -13,7 +14,7 @@ using System.Threading.Tasks;
 namespace PageRendrAPI.Controllers
 {
     [ApiController]
-    [Route("renderpage")]   
+    [Route("renderpage")]
     public class RenderController : ControllerBase
     {
         private readonly SeleniumBrowser Browser;
@@ -26,22 +27,25 @@ namespace PageRendrAPI.Controllers
             HttpClient = httpClient;
             ApiClient = apiClient;
         }
-        Dictionary<string, Tuple<string, DateTime>> Cache = new();
+        private static readonly Dictionary<string, Tuple<string, DateTime>> Cache = new();
 
         [HttpGet]
         [ClientIPAddressFilter]
         public async Task<IActionResult> Get(string url = "https://silverdiamond.cf")
         {
-            if(Cache.TryGetValue(url, out var thing))
+            Debug.WriteLine(Cache.Count == 1 ? "There is {0} thing in cache" : "There are {0} things in the cache", Cache.Count);
+            if (Cache.TryGetValue(url, out var thing))
             {
-                if((DateTime.Now - thing.Item2 )<TimeSpan.FromMinutes(2))
+                Debug.WriteLine("Cache is {0} old", DateTime.Now - thing.Item2);
+                if ((DateTime.Now - thing.Item2) < TimeSpan.FromMinutes(10))
                 {
+                    Debug.WriteLine("Used url from cache");
                     return Redirect(thing.Item1);
                 }
             }
             var imageEndpoint = new ImageEndpoint(ApiClient, HttpClient);
             var imageUpload = await imageEndpoint.UploadImageAsync(new MemoryStream(await Browser.RenderUrlAsyncAsByteArray(url)));
-            if(Cache.ContainsKey(url))
+            if (Cache.ContainsKey(url))
             {
                 Cache.Remove(url);
             }
