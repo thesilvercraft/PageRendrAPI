f17d018fdc63ce6959699e4079a2267e6cabe779
SilverDimond <46320280+Silverdimond@users.noreply.github.com>
2021-09-27 12:09:44 +0200

    Gaming and caching

--- PageRendrAPI/Controllers/RenderController.cs
+++ PageRendrAPI/Controllers/RenderController.cs
@@ -26,13 +26,26 @@ namespace PageRendrAPI.Controllers
             HttpClient = httpClient;
             ApiClient = apiClient;
         }
+        Dictionary<string, Tuple<string, DateTime>> Cache = new();
 
         [HttpGet]
         [ClientIPAddressFilter]
         public async Task<IActionResult> Get(string url = "https://silverdiamond.cf")
         {
+            if(Cache.TryGetValue(url, out var thing))
+            {
+                if((DateTime.Now - thing.Item2 )>TimeSpan.FromMinutes(2))
+                {
+                    return Redirect(thing.Item1);
+                }
+            }
             var imageEndpoint = new ImageEndpoint(ApiClient, HttpClient);
             var imageUpload = await imageEndpoint.UploadImageAsync(new MemoryStream(await Browser.RenderUrlAsyncAsByteArray(url)));
+            if(Cache.ContainsKey(url))
+            {
+                Cache.Remove(url);
+            }
+            Cache.Add(url, new Tuple<string, DateTime>(imageUpload.Link, DateTime.Now));
             return Redirect(imageUpload.Link);
         }
     }
